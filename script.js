4657dd1d6390f222fe3e54ccea04c4af920d6438
Bereketmebrahtu <mebrahtubereket@gmail.com>
2022-03-12 23:49:06 +0000

    HTML button added with a span, function wrote to generate random colors using Math.random method

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const colors = []
+const Btn = document.querySelector('.Btn')
+const colorEl = document.querySelector('#color')
+
+
+const changeBackground = () => {
+const randomColor = Math.floor(Math.random()* 16777216).toString(16);
+document.body.style.backgroundColor = "#" + randomColor;
+colorEl.innerHTML = "#" + randomColor;
+};
+
+Btn.addEventListener("click", changeBackground);
+changeBackground()
